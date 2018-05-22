292ef4a0dd887202573da37a3e6dc9f9419031cc
v1ncee <duchateau.vince@gmail.com>
2018-05-22 16:44:54 +0200

    Added services and menulinks

--- modules/custom/thomas_more_social_media/src/Plugin/Block/SocialMediaBlock.php
+++ modules/custom/thomas_more_social_media/src/Plugin/Block/SocialMediaBlock.php
@@ -17,17 +17,17 @@ class SocialMediaBlock extends BlockBase {
 
   /**
    * {@inheritdoc}
-   */
+   **/
 
   public function build() {
     return [
       '#theme' => 'social-media',
       '#attached' => ['library' => 'thomas_more_social_media/social_media'],
-      '#facebook_url' => $this->state->get('thomas_more_social_media.facebook_url'),
-      '#google_plus_url' => $this->state->get('thomas_more_social_media.google_plus_url'),
-      '#twitter_url' => $this->state->get('thomas_more_social_media.twitter_url'),
-      '#linkedin_url' => $this->state->get('thomas_more_social_media.linkedin_url'),
-      '#foursquare_url' => $this->state->get('thomas_more_social_media.foursquare_url'),
+      '#facebook_url' => \Drupal::state()->get('thomas_more_social_media.facebook_url'),
+      '#google_plus_url' => \Drupal::state()->get('thomas_more_social_media.google_plus_url'),
+      '#twitter_url' => \Drupal::state()->get('thomas_more_social_media.twitter_url'),
+      '#linkedin_url' => \Drupal::state()->get('thomas_more_social_media.linkedin_url'),
+      '#foursquare_url' => \Drupal::state()->get('thomas_more_social_media.foursquare_url'),
     ];
   }
 
