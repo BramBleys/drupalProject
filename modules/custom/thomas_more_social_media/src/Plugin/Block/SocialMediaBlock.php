331815ca59d12420cca1eba638659396cd59039f
v1ncee <duchateau.vince@gmail.com>
2018-05-22 11:22:31 +0200

    Initial commit

---
+++ modules/custom/thomas_more_social_media/src/Plugin/Block/SocialMediaBlock.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Drupal\thomas_more_social_media\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Social Media' Block.
+ *
+ * @Block(
+ *   id = "thomas_more_social_media_block",
+ *   admin_label = @Translation("Social Media"),
+ * )
+ */
+
+class SocialMediaBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+
+  public function build() {
+
+  }
+
+}
